29165025b6e12cd9b25e5ec998ed49734d65ee4a
ntn1999 <nam14nt@gmail.com>
2025-05-04 15:11:48 +0700

    init project

---
+++ src/main/java/com/example/shopnam/ShopnamApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopnamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopnam/ShopnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
